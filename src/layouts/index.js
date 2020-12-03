d1f0ed3095e3e88df05f70303177ddebb23c1680
Liliripe <liliripe.studio@gmail.com>
2020-12-03 14:55:30 -0600

    init styled components

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { ThemeProvider } from 'styled-components'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import GlobalStyle from '../styles/global'
+import theme from '../styles/theme'
 
 const Layout = ({ children }) => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <div className="siteContent">
         {children}
       </div>
-    </>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
 
